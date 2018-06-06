a804b37ebc0ab578a7ed15953c0453e191289375
2017TJM <32608701+2017TJM@users.noreply.github.com>
2018-06-06 16:19:27 +0000

    Create vedio.py

---
+++ vedio.py
@@ -0,0 +1,23 @@
+import cv2
+cap = cv2.VideoCapture(0)
+#fourcc = cv2.cv.FOURCC(*'XVID')
+#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+       # frame = cv2.flip(frame,0)
+        # write the flipped frame
+        #out.write(frame)
+        cv2.line(frame, (0, 0), (511, 511), (255, 0, 0), 5)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(2) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
